0e7a89ac157211975f2cae10dcc095c97f4b7165
fnica2310 <fnieto1@inscamidemar.cat>
2023-11-22 18:29:48 +0100

    aaa

---
+++ p4/src/main/java/m03/uf4/p4/p4/P4Application.java
@@ -0,0 +1,10 @@
+package m03.uf4.p4.p4;
+
+
+public class P4Application {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ p4/src/test/java/m03/uf4/p4/p4/P4ApplicationTests.java
@@ -0,0 +1,13 @@
+package m03.uf4.p4.p4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
